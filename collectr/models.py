6b3f54d21e198ab66d5d8b5053eeef62a75a1aa8
Cory Benfield <lukasaoz@gmail.com>
2013-02-26 18:41:57 +1100

    Add some initialization.

--- collectr/models.py
+++ collectr/models.py
@@ -19,4 +19,31 @@ class StaticDir(object):
     :param dir: The root of the directory tree.
     """
     def __init__(self, dir):
-        raise NotImplementedError("Not yet implemented.")
+        #: A string representing the location of static files that have yet to
+        #: be minified. Files in this directory will be minified, and the
+        #: minified versions will be saved to 'dir'.
+        self.input_directory = None
+
+        #: The minifier to use on CSS and/or Javascript files. May be a
+        #: dictionary whose keys correspond to file extensions or a string. The
+        #: string is considered a special case, and will be applied to both
+        #: Javascript and CSS files.
+        #: If the dictionary is used, the keys correspond to a command-line to
+        #: run. This should be a python format string with two string
+        #: variables, %{in}s and %{out}s. These refer to the input and output
+        #: filename respectively.
+        self.minifier = {'css': None, 'js': None}
+
+        #: Whether to update all files, regardless of whether they have been
+        #: changed.
+        self.force_update = False
+
+        #: Files to ignore. Should be a list of regular expressions. Everything
+        #: that matches any of these regular expressions will be totally
+        #: ignored. The regex will be applied to the relative path.
+        self.ignore = []
+
+        #: A dictionary of keys and values that correspond to the metadata that
+        #: should be applied to the files. This metadata will be applied to
+        #: _all_ the files found by this :class:`StaticDir <StaticDir>`.
+        self.metadata = {}
