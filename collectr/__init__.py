9468d08cf0a0c259be07425472b9ee0234241eb5
Cory Benfield <lukasaoz@gmail.com>
2013-02-26 16:43:29 +1100

    Probably should put some stuff here.

---
+++ collectr/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+collectr
+--------
+
+collectr is static file management for S3, for human beings. See README.md for
+more.
+
+:copyright: (c) 2013 Cory Benfield
+:license: MIT, see LICENSE for details.
+"""
+
+__title__ = 'collectr'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Cory Benfield'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Cory Benfield'
+
+from .api import *
+from .models import StaticDir
